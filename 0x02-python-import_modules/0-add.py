3a15debeb5c6e0758b422ac1fe304358eb55d438
Joelmwangi <joelmwangi515@gmail.com>
2023-03-15 07:28:16 -0700

    coding modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    form add_0 import add_0
+    x = 4
+    y = 6
+    print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
