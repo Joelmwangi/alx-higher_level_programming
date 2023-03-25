4d5989af2559a728ea2847b55c28f12692ddafa6
Joelmwangi <joelmwangi515@gmail.com>
2023-03-25 06:29:34 -0700

    am here

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,8 +5,3 @@ if __name__ == "__main__":
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
-if __name__ == "__main__":
-    form add_0 import add_0
-    x = 4
-    y = 6
-    print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
